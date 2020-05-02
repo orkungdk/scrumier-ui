f315e3e04e3e1c1178b711cd224d0a77c242d663
Orkun Gedik <orkungedik90@gmail.com>
2020-05-02 04:47:09 +0300

    Initial Commit

---
+++ pages/authentication/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>ORKUN</span>
+    <footer>
+      <small>
+        <em>&mdash;John Johnson</em>
+      </small>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped></style>
---
+++ pages/authentication/register.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <small>
+      <em>&mdash;John Johnson</em>
+    </small>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Register'
+}
+</script>
+
+<style scoped></style>
